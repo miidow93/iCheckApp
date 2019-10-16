e535954b472f9dd0ace2ca1374669d48fd96f910
miidow93 <mohammedjellab@gmail.com>
2019-10-16 16:22:15 +0100

    add checklist model

--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -5,15 +5,20 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class DataService {
+
   private enginDataSource = new BehaviorSubject<any[]>([]);
   currentEnginDataSource = this.enginDataSource.asObservable();
+
   private conducteurDataSource = new BehaviorSubject<any[]>([]);
   currentConducteurDataSource = this.conducteurDataSource.asObservable();
+
   constructor() { }
+
   changeEnginDataSource(data) {
     console.log('Engin Data Service: ', data);
     this.enginDataSource.next([...data]);
   }
+  
   changeConducteurDataSource(data) {
     console.log('conducteur Data Service: ', data);
     this.conducteurDataSource.next([...data]);
