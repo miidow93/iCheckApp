ab9d61d8f387eb877e1e3387745b5a36144f14b8
miidow93 <mohammedjellab@gmail.com>
2019-11-13 15:34:04 +0100

    first test build

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,6 +1,9 @@
 export enum Constants {
-    api = 'http://localhost:2229/api/',
-    serverImg = 'http://localhost:2229/'
+    // api = 'http://localhost:2229/api/',
+    // serverImg = 'http://localhost:2229/'
+
+    api = 'http://192.168.1.20:1070/api/',
+    serverImg = 'http://192.168.1.20:1070/'
 
     // api = 'http://192.168.100.213:1050/api/',
     // serverImg = 'http://192.168.100.213:1050/'
