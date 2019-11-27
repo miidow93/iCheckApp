6641d5585d7ce634af3f9544f3cf750fd3279af3
miidow93 <mohammedjellab@gmail.com>
2019-11-27 11:23:22 +0100

    change toolbar color

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,6 +1,6 @@
 export enum Constants {
-    // api = 'http://192.168.1.6:5000/api/',
-    // serverImg = 'http://192.168.1.6:5000/'
+    api = 'http://192.168.1.11:5000/api/',
+    serverImg = 'http://192.168.1.11:5000/'
 
     // api = 'http://192.168.1.20:1070/api/',
     // serverImg = 'http://192.168.1.20:1070/'
@@ -8,7 +8,7 @@ export enum Constants {
     // api = 'http://192.168.100.213:1050/api/',
     // serverImg = 'http://192.168.100.213:1050/'
 
-    api = ' http://10.54.7.216:1070/api/', // Settat
-    serverImg = ' http://10.54.7.216:1070/' // Settat
+    // api = ' http://10.54.7.216:1070/api/', // Settat
+    // serverImg = ' http://10.54.7.216:1070/' // Settat
 
 }
