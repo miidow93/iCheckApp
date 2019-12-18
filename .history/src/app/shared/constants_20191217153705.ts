0f3481d0c9ddb379306ee4dc2dd982ed5b92b212
ayoubelhafi <ucseme@gmail.com>
2019-12-18 12:30:28 +0100

    users style

---
+++ .history/src/app/shared/constants_20191217153705.ts
@@ -0,0 +1,20 @@
+export enum Constants {
+    // api = 'http://192.168.1.10:5000/api/',
+    // serverImg = 'http://192.168.1.10:5000/'
+
+    api = 'http://localhost:2229/api/',
+    serverImg = 'http://localhost:2229/',
+
+    // api = 'http://192.168.1.20:1070/api/',
+    // serverImg = 'http://192.168.1.20:1070/'
+
+    // api = 'http://192.168.100.213:1050/api/',
+    // serverImg = 'http://192.168.100.213:1050/'
+
+    // api = ' http://10.54.7.216:1070/api/', // Settat
+    // serverImg = ' http://10.54.7.216:1070/' // Settat
+   
+    // img_OurVisitor: './assets/images/LOGOVISITEUR.png',
+    // img_Cms = './assets/images/CMS.jpg'
+
+}
