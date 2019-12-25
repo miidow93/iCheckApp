7c5281605da916fb0f9afbc2b3ce1566c065e466
medjellab <ats.medjellab@gmail.com>
2019-12-25 16:17:09 +0100

    fix citerne and plateau check

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,6 +1,6 @@
 export enum Constants {
-    api = 'http://192.168.1.13:5000/api/',
-    serverImg = 'http://192.168.1.13:5000/'
+    api = 'http://192.168.1.21:5000/api/',
+    serverImg = 'http://192.168.1.21:5000/'
 
     // api = 'http://localhost:2229/api/',
     // serverImg = 'http://localhost:2229/',
